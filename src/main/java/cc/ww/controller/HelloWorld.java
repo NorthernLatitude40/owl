a1960d2bb90e20d7112a8745105e6bc0c45594a2
ww <wang_123333@sina.com>
2019-09-06 22:40:29 +0800

    hello

--- src/main/java/cc/ww/controller/HelloWorld.java
+++ src/main/java/cc/ww/controller/HelloWorld.java
@@ -1,4 +1,13 @@
 package cc.ww.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/owl")
 public class HelloWorld {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public void helloWorld() {
+
+    }
 }
