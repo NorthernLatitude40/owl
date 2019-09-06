e70c9bf7960974ba3983f5ac6ab96b691bfe5426
ww <wang_123333@sina.com>
2019-09-06 21:07:55 +0800

    first commit

---
+++ src/main/java/cc/ww/owl/OwlApplication.java
@@ -0,0 +1,13 @@
+package cc.ww.owl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlApplication.class, args);
+	}
+
+}
